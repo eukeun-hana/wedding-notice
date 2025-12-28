977725b9ebc3835e1de4e2d145ab3da8d78398da
eukeun-hana <connetflix@naver.com>
2025-12-28 22:13:36 +0900

    update wedding info

--- src/const.ts
+++ src/const.ts
@@ -37,16 +37,16 @@ export const NMAP_PLACE_ID = 13321741
 // 예: https://place.map.kakao.com/8634826 -> 8634826
 export const KMAP_PLACE_ID = 8634826
 
-export const BRIDE_FULLNAME = "정지원"
-export const BRIDE_FIRSTNAME = "지원"
+export const BRIDE_FULLNAME = "이하나"
+export const BRIDE_FIRSTNAME = "하나"
 export const BRIDE_TITLE = "장녀"
-export const BRIDE_FATHER = "정상원"
-export const BRIDE_MOTHER = "박윤정"
+export const BRIDE_FATHER = "이용기"
+export const BRIDE_MOTHER = "신혜영"
 export const BRIDE_INFO = [
   {
     relation: "신부",
     name: BRIDE_FULLNAME,
-    phone: "010-0000-0000",
+    phone: "010-8867-2534",
     account: "우리은행 0000000000000",
   },
   {
@@ -63,28 +63,28 @@ export const BRIDE_INFO = [
   },
 ]
 
-export const GROOM_FULLNAME = "남주호"
-export const GROOM_FIRSTNAME = "주호"
-export const GROOM_TITLE = "차남"
-export const GROOM_FATHER = "남현태"
-export const GROOM_MOTHER = "김현영"
+export const GROOM_FULLNAME = "노의건"
+export const GROOM_FIRSTNAME = "의건"
+export const GROOM_TITLE = "장남"
+export const GROOM_FATHER = "노재이"
+export const GROOM_MOTHER = "박하영"
 export const GROOM_INFO = [
   {
     relation: "신랑",
     name: GROOM_FULLNAME,
-    phone: "010-0000-0000",
+    phone: "010-4414-9016",
     account: "하나은행 00000000000000",
   },
   {
     relation: "신랑 아버지",
     name: GROOM_FATHER,
-    phone: "010-0000-0000",
+    phone: "010-2854-0419",
     account: "신한은행 000000000000",
   },
   {
     relation: "신랑 어머니",
     name: GROOM_MOTHER,
-    phone: "010-0000-0000",
+    phone: "010-5116-9016",
     account: "국민은행 000000000000",
   },
 ]
