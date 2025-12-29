dd5985793eda70ddb52e20faf7fd75e72cd463ae
eukeun-hana <connetflix@naver.com>
2025-12-29 22:25:37 +0900

    Update env.ts

--- src/env.ts
+++ src/env.ts
@@ -1,4 +1,4 @@
-export const NAVER_MAP_CLIENT_ID = import.meta.env.VITE_NAVER_MAP_CLIENT_ID
-export const KAKAO_SDK_JS_KEY = import.meta.env.VITE_KAKAO_SDK_JS_KEY
-export const SERVER_URL = import.meta.env.VITE_SERVER_URL
-export const STATIC_ONLY = import.meta.env.VITE_STATIC_ONLY === "true"
+export const NAVER_MAP_CLIENT_ID = ""
+export const KAKAO_SDK_JS_KEY = "3e0929bbec4eb0b003f0e15a5b6b7485"
+export const SERVER_URL = "https://wedding-notice-server.onrender.com/"
+export const STATIC_ONLY = false
